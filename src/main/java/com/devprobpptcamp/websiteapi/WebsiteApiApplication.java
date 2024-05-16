15ab144106f9f905be23aaef3507cf8385cab143
devprobootcamp <167482994+devprobootcamp@users.noreply.github.com>
2024-05-15 20:45:45 -0500

    FIrst Commit

---
+++ src/main/java/com/devprobpptcamp/websiteapi/WebsiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.devprobpptcamp.websiteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devprobpptcamp/websiteapi/WebsiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devprobpptcamp.websiteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
